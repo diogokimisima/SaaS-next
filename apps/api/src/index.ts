161ef5438ac481b7ceea87b6b892be42fd367b4e
dkimisima@gmail.com <dkimisima@gmail.com>
2024-12-24 16:14:32 -0300

    otimizando as tipagens das permissoes

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,6 @@
 import { defineAbilityFor } from '@saas/auth'
 
-const ability = defineAbilityFor({ role: 'ADMIN' })
+const ability = defineAbilityFor({ role: 'MEMBER' })
 
 const userCanInviteSomeoneElse = ability.can('invite', 'User')
 const userCanDeleteOtherUsers = ability.can('delete', 'User')

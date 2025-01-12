abdf3441b803f04954f95565d6602085465f25a3
dkimisima@gmail.com <dkimisima@gmail.com>
2025-01-12 15:39:01 -0300

    associando o usuario a uma organizacao caso tiver o @email a organizacao existente

--- apps/api/src/http/routes/auth/create-account..ts
+++ apps/api/src/http/routes/auth/create-account..ts
@@ -32,12 +32,28 @@ export async function createAccount(app: FastifyInstance) {
           .send({ message: 'user with same email already exists' })
       }
 
+      const [, domain] = email.split('@')
+
+      const autoJoinOrganization = await prisma.organization.findFirst({
+        where: {
+          domain,
+          shouldAttachUsersByDomain: true,
+        },
+      })
+
       const passwordHash = await hash(password, 6)
       await prisma.user.create({
         data: {
           name,
           email,
           passwordHash,
+          member_on: autoJoinOrganization
+            ? {
+                create: {
+                  organizationId: autoJoinOrganization.id,
+                },
+              }
+            : undefined,
         },
       })
 

2d9ffcc0ea15ef5db8fae6c3b31f3acc6fd2c1c6
dkimisima@gmail.com <dkimisima@gmail.com>
2025-02-20 22:13:02 -0300

    Estados de erro do formulario com useActionState

---
+++ apps/web/src/app/auth/sign-in/sign-in-form.tsx
@@ -0,0 +1,84 @@
+'use client'
+
+import { AlertTriangle, Loader2 } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useActionState } from 'react'
+
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Separator } from '@/components/ui/separator'
+
+import githubIcon from '../../../assets/github-icon.svg'
+import { signInWithEmailAndPassword } from './actions'
+
+export function SignInForm() {
+  const [{ success, message, errors }, formAction, isPending] = useActionState(
+    signInWithEmailAndPassword,
+    { success: false, message: null, errors: null },
+  )
+
+  return (
+    <form action={formAction} className="space-y-4">
+      {success === false && message && (
+        <Alert variant="destructive">
+          <AlertTriangle className="size-4" />
+          <AlertTitle>Sign in failed!</AlertTitle>
+          <AlertDescription>{message}</AlertDescription>
+        </Alert>
+      )}
+
+      <div className="space-y-1">
+        <Label htmlFor="email">E-mail</Label>
+        <Input name="email" type="text" id="email" />
+
+        {errors?.email && (
+          <p className="text-xs font-medium text-red-500 dark:text-red-400">
+            {errors.email[0]}
+          </p>
+        )}
+      </div>
+
+      <div className="space-y-1">
+        <Label htmlFor="password">Password</Label>
+        <Input name="password" type="password" id="password" />
+
+        {errors?.password && (
+          <p className="text-xs font-medium text-red-500 dark:text-red-400">
+            {errors.password[0]}
+          </p>
+        )}
+
+        <Link
+          href="/auth/forgot-password"
+          className="text-foreground-muted text-xs font-medium hover:underline"
+        >
+          Forgot yout password
+        </Link>
+      </div>
+
+      <Button type="submit" className="w-full" disabled={isPending}>
+        {isPending ? (
+          <Loader2 className="size-4 animate-spin" />
+        ) : (
+          'Sign in with e-mail'
+        )}
+      </Button>
+
+      <Button variant="link" className="w-full" asChild size="sm">
+        <Link href="/auth/sign-up" className="w-full">
+          Create new account
+        </Link>
+      </Button>
+
+      <Separator />
+
+      <Button type="submit" variant="outline" className="w-full">
+        <Image src={githubIcon} className="size-5 dark:invert" alt="" />
+        Sign in with Github
+      </Button>
+    </form>
+  )
+}
